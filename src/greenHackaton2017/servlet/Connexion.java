801ece1da7a2fb5309d7de46fd89450c7993b427
Clément Begnaud <clement.begnaud@gmail.com>
2017-11-22 20:59:22 +0100

    [ADD] Bootstrap theme, accueil.jsp, index.jsp, Connexion.java

---
+++ src/greenHackaton2017/servlet/Connexion.java
@@ -0,0 +1,32 @@
+package greenHackaton2017.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Connexion
+ */
+@WebServlet(description = "Servlet appelé pour une connexion d'un utilisateur enregistré", urlPatterns = { "/Connexion" })
+public class Connexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Connexion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
