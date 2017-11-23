b779e48d1839e150970098bca80f04cf30c293c8
Sorlie <sorlie.pierrefrancois@gmail.com>
2017-11-23 03:52:39 +0100

    ADD - BO/DAO Implementation for User Managing

--- src/greenHackaton2017/java/bo/UserBO.java
+++ src/greenHackaton2017/java/bo/UserBO.java
@@ -1,5 +1,27 @@
 package greenHackaton2017.java.bo;
 
-public class UserBO {
+import greenHackaton2017.java.dao.UserDAO;
+import greenHackaton2017.java.model.User;
 
+public class UserBO {
+	
+	private UserDAO userDAO = new UserDAO();
+	
+	private UserBO() {
+		
+	}
+	
+	public void addUser(String login, String password) {
+		userDAO.createUser(new User(login, password));
+	}
+	
+	public User getUser(String login, String password) {
+		User possibleUser = userDAO.getUser(login);
+		if(possibleUser.getPassword().equals(password)) {
+			return possibleUser;
+		} else {
+			return null;
+		}
+	}
+	
 }
